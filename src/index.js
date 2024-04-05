a99ef0ce9e8a9dec4fb04932ea9d5e2d87e62426
Ahella-Bassem <ahellabassem12@gmail.com>
2024-04-06 00:56:17 +0200

    same as old project

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import {BrowserRouter} from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './redux/store'
-import { ToastContainer} from "react-toastify";
+import "./index.css";
+import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+
+import store from "./redux/store";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Provider  store={store}>
-      <App />
-      <ToastContainer/>
-    </Provider>
+      <Provider store={store}>
+        <App />
+        <ToastContainer />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
-
